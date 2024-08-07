d1bd3da58cb5bfcdefbb78da44482ea6350dbc95
高帅博 <gaoshuaibo@luoyanmindeMac-mini.local>
2024-08-07 18:15:18 +0800

    master line1

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("hello world");
+    printf("This is master branch");
     printf("hello world");
     printf("hello world");
     printf("hello world");
