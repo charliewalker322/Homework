bdd3805ab91b5444405d0882591edcb33437c0ab
高帅博 <gaoshuaibo@luoyanmindeMac-mini.local>
2024-08-07 18:16:41 +0800

    add feature

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("This is master branch");
+    printf("This is local feature branch");
     printf("hello world");
     printf("hello world");
     printf("hello world");
