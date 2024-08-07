1eb55698786309f3d094f3ee178b57c637475405
高帅博 <gaoshuaibo@luoyanmindeMac-mini.local>
2024-08-07 19:34:33 +0800

    revise line1

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("This is master branch");
+    printf("This is the master branch");
     printf("hello world master!");
     printf("hello world");
     printf("hello world");
